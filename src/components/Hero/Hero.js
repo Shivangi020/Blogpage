2354ab3f5fd71bff06e76f0830de69fcb294f263
Shivangi Singh <me.shivasingh.0987@gmail.com>
2021-05-31 16:02:21 +0530

    adding files

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Hero.css"
+/**
+* @author Shivangi
+* @function Hero
+**/
+
+const Hero = (props) => {
+  return(
+    <div><div class="header_container">Header</div></div>
+   )
+
+ }
+
+export default Hero
